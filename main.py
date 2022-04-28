8ff05183fcc348a1cbd359a48713d43034bdf7f0
Daniel Abraham <daniel_abraham1@berkeley.edu>
2022-04-28 02:31:37 -0700

    Need to impliment batches

--- main.py
+++ main.py
@@ -6,34 +6,36 @@ from myNN import myNN
 n_train = 100000
 n_test = 100
 lr = 1e-3
-dims = [2,1]
-act_types = ['relu']
+dims = [2,7,7,3,1]
+act_types = ['relu', 'relu', 'relu', 'sig']
 def f_true(x):
-	a = 0 + (x[0] > 0.5)
-	b = 0 + (x[1] > 0.5)
-	return 0 + (a & b)
+	# 1 if on disc, 0 else
+	r_max = 0.8
+	r_min = 0.2 
+	r = np.linalg.norm(x) 
+	return 0 + (r_min < r) * (r < r_max)
 
 # ----------------- Benchmark -----------------
 # Create neural net
 nn = myNN(dims, act_types, lr=1e-3)
 
 # Generate inputs
-inps = np.random.randint(0, 2, (n_train, 2))
+inps = np.random.uniform(-1, 1, (n_train, 2))
 
 # Train, keep track of mses
 mses = []
 for i, inp in enumerate(inps):
 	nn.forward(inp)
 	mse = nn.compare_loss(f_true(inp), back_prop=True)
-	if i % 100 == 0:
+	if i % (n_train//100) == 0:
 		mses.append(mse)
 
 # Create grid to visualize network
-x = np.linspace(0, 1, 100)
+x = np.linspace(-1, 1, n_test)
 X, Y = np.meshgrid(x, x)
 Z = np.zeros_like(X)
-for r in range(100):
-	for c in range(100):
+for r in range(n_test):
+	for c in range(n_test):
 		arr = np.array([X[r,c], Y[r,c]])
 		Z[r,c] = nn.forward(arr).squeeze()
 
@@ -41,5 +43,5 @@ for r in range(100):
 plt.subplot(121)
 plt.plot(mses)
 plt.subplot(122)
-plt.imshow(Z, origin='lower')
+plt.imshow(Z, origin='lower', extent=[-1,1]*2)
 plt.show()
